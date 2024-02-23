cf92bf4165c27b507c2f880b50ce9991715bafa3
Hariom vishwkarma <vishwakarmahariom27@gmail.com>
2024-02-24 00:12:42 +0530

    chat function working for individuals

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+var http = require("http");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+//for middleware
+app.use(express.json());
+var clients = {};
+
+io.on("connection",(socket) => {
+    console.log("connected");
+    console.log(socket.id," has joined");
+    socket.on("signin",(id)=>{
+        console.log(id);
+        clients[id] = socket;
+        console.log(clients);
+    });
+    socket.on("message",(msg) => {
+        console.log(msg);
+        let targetId = msg.targetId;
+        clients[targetId].emit("message",msg);
+    });
+})
+
+server.listen(port,"0.0.0.0",() =>{
+    console.log("Server Started");
+});
